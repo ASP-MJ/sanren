b2b225451f84593b82519e29bc4fcc25e7a66de7
n199-jin <matsuda.j@aspcs.co.jp>
2026-05-22 10:29:26 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -100,7 +100,7 @@ function ParticipantView({ state, myName, setMyName, guesses, setGuesses, submit
     <motion.div initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }}>
        <div className="flex-between" style={{ marginBottom: '30px' }}>
         <h2 className="brand-title">
-          SANRENTAN PRO
+          SANRENTAN
         </h2>
       </div>
 
